8aa3cd6727a6d3789a7e930ca0680d206f3509a7
Jan Koszewski <jan.koszewski@US-JKOSZE02-OSX.local>
2015-11-12 17:29:25 -0500

    Moved add_option.js to polls/new, wrapped function in .ready

--- app/assets/javascripts/add_option.js
+++
@@ -1,5 +0,0 @@
-var $options = $('.options-fields')
-
-$('#add-option').on('click', function(){
-	$("<label>Option</label><input type='text' name='poll[options][]' id='poll_options'></input>").appendTo($options);
-});
